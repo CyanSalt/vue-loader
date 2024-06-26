e53dec66fc0d241239f76f2150bc5e9a6ee7d6af
CyanSalt <c.salt@hotmail.com>
2024-06-26 10:30:43 +0800

    fix: es5 syntax runtime

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,5 +17,13 @@ export default config({
         'unicorn/prefer-optional-catch-binding': 'off',
       },
     },
+    {
+      files: ['lib/runtime/*.js'],
+      rules: {
+        'no-var': 'off',
+        'object-shorthand': ['warn', 'never'],
+        '@stylistic/ts/comma-dangle': ['warn', 'never'],
+      },
+    },
   ],
 })
